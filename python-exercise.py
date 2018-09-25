bc9c93e6632db05810486aea81ee319010b09952
rinoale <rinoale@hanmail.net>
2018-09-25 12:21:21 +0900

    array reference

--- python-exercise.py
+++ python-exercise.py
@@ -80,3 +80,17 @@ if 100 in r:
     print('exist')
 else:
     print('not exist')
+
+i = [1, 2, 3, 4, 5]
+j = i
+j[0] = 100
+print('i = ', i)
+print('j = ', j)
+
+x = [1, 2, 3, 4, 5]
+# y = x.copy()
+y = x[:]
+
+x[0] = 100
+print('x = ', x)
+print('y = ', y)

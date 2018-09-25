67db635f7fbb8bd0434b15b39884a14b365eb76d
rinoale <rinoale@hanmail.net>
2018-09-25 11:35:44 +0900

    string index

--- python-exercise.py
+++ python-exercise.py
@@ -16,3 +16,32 @@ print('Hi', 'Mike', sep=',', end='\n')
 print(17//3)
 print(17%3)
 print(5**2)
+
+print("""
+line1
+line2
+line3
+""")
+
+print("""\
+line1
+line2
+line3\
+""")
+
+print('Hi.' * 3 + 'Mike.')
+print('Py''thon')
+
+s = ('aaaaaaaaaaaaaaaaaaaaaaaa'
+    'bbbbbbbbbbbbbbbbbbbbbbbb')
+
+print(s)
+
+word = 'python'
+
+print(word[0:2])
+print(word[:2])
+
+word = 'j' + word[1:]
+
+print(word)

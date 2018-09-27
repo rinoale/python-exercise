6531143f6fe0ff001e0fda8fe2ba33943142c8fd
rinoale <rinoale@hanmail.net>
2018-09-27 17:44:01 +0900

    decorator

--- python-exercise.py
+++ python-exercise.py
@@ -167,3 +167,19 @@ is_empty = None
 
 if is_empty is None:
     print('None')
+
+
+def print_info(func):
+    def wrapper(*args, **kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        print('end')
+        return result
+    return wrapper
+
+@print_info
+def add_num(a, b):
+    return a+b
+
+r = add_num(10, 20)
+print(r)

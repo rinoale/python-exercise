be3acd034c178b26441648e4efb44465ae0b60f4
rinoale <rinoale@hanmail.net>
2018-09-27 18:12:04 +0900

    generator

--- zip.py
+++ zip.py
@@ -14,3 +14,15 @@ print(d)
 d = {i for i in range(10) if i%2 == 0}
 
 print(d)
+
+def g():
+    for i in range(10):
+        yield i
+
+g = g()
+
+print(type(g))
+
+g = (i for i in range(10))
+
+print(type(g))
